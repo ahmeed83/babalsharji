93f63ade84876417c25e9db20939d59d2b36e0e5
Ahmed Aziz <ahmed83@me.com>
2015-02-13 12:28:12 +0100

    intelliJ fixes voor Ahmed

---
+++ BabalSharji/target/generated-sources/annotations/com/babalsharji/entity/Advertisements_.java
@@ -0,0 +1,15 @@
+package com.babalsharji.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-02-13T12:26:39")
+@StaticMetamodel(Advertisements.class)
+public class Advertisements_ { 
+
+    public static volatile SingularAttribute<Advertisements, String> price;
+    public static volatile SingularAttribute<Advertisements, String> name;
+    public static volatile SingularAttribute<Advertisements, Integer> id;
+
+}
---
+++ BabalSharji/target/generated-sources/annotations/com/babalsharji/entity/Users_.java
@@ -0,0 +1,23 @@
+package com.babalsharji.entity;
+
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-02-13T12:26:39")
+@StaticMetamodel(Users.class)
+public class Users_ { 
+
+    public static volatile SingularAttribute<Users, Date> dateofbirth;
+    public static volatile SingularAttribute<Users, String> firstname;
+    public static volatile SingularAttribute<Users, String> password;
+    public static volatile SingularAttribute<Users, String> address;
+    public static volatile SingularAttribute<Users, String> gender;
+    public static volatile SingularAttribute<Users, String> photo;
+    public static volatile SingularAttribute<Users, String> telephone;
+    public static volatile SingularAttribute<Users, Integer> id;
+    public static volatile SingularAttribute<Users, String> email;
+    public static volatile SingularAttribute<Users, String> lastname;
+
+}
