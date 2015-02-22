915fc7212bbde7ecdf4e6d16edfdad7e51dd6569
Teije van Sloten <teije-vs@hotmail.com>
2015-02-22 21:33:57 +0100

    Encryption improved

--- BabalSharji/src/main/java/com/babalsharji/util/Encryption.java
+++ BabalSharji/src/main/java/com/babalsharji/util/Encryption.java
@@ -2,35 +2,42 @@ package com.babalsharji.util;
 
 import java.security.InvalidKeyException;
 import java.security.Key;
-import java.security.NoSuchAlgorithmException;
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
 
 public class Encryption {
+
     private final String key = "FA8ebejabePaWugU";
     private static Encryption INSTANCE_VARIABLE = null;
-    
-    private Encryption(){}
-    
-    public static Encryption getInstance(){
-        if(INSTANCE_VARIABLE == null) {
+    private Key aesKey;
+    private Cipher cipher;
+
+    private Encryption() {
+        try {
+            aesKey = new SecretKeySpec(key.getBytes(), "AES");
+            cipher = Cipher.getInstance("AES");
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    public static Encryption getInstance() {
+        if (INSTANCE_VARIABLE == null) {
             INSTANCE_VARIABLE = new Encryption();
         }
         return INSTANCE_VARIABLE;
     }
-    
+
     public String encryptPassword(String newPassword) {
         try {
-            Key aesKey = new SecretKeySpec(key.getBytes(), "AES");
-            Cipher cipher = Cipher.getInstance("AES");
             cipher.init(Cipher.ENCRYPT_MODE, aesKey);
             byte[] encrypted = cipher.doFinal(newPassword.getBytes());
 
             return new String(encrypted);
-        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
+        } catch (InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
             e.printStackTrace();
             throw new IllegalArgumentException();
         }
@@ -38,14 +45,12 @@ public class Encryption {
 
     public String decryptPassword(String encryptedPassword) {
         try {
-            Key aesKey = new SecretKeySpec(key.getBytes(), "AES");
-            Cipher cipher = Cipher.getInstance("AES");
             cipher.init(Cipher.DECRYPT_MODE, aesKey);
             byte[] encrypted = cipher.doFinal(encryptedPassword.getBytes());
             String decrypted = new String(cipher.doFinal(encrypted));
 
             return decrypted;
-        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
+        } catch (InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
             e.printStackTrace();
             throw new IllegalArgumentException();
         }
